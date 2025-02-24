ae1635f36dfc138bd40b1a9b63e77b46fce5ca13
Brian Ketelsen <bketelsen@gmail.com>
2025-02-24 15:08:49 +0000

    chore: log environment call

--- tint/env.go
+++ tint/env.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -14,8 +15,8 @@ var lvl = new(slog.LevelVar)
 func Initialize(w io.Writer, debug bool) {
 
 	exe, _ := os.Executable()
+	exe = filepath.Base(exe)
 	debugEnv := fmt.Sprintf("%s_DEBUG", strings.ToUpper(exe))
-	slog.Info("Checking environment", "debug", debugEnv)
 	_, ok := os.LookupEnv(debugEnv)
 	if ok {
 		debug = true
